14e60ea377437a3f0cc91f8907c3dba80c803812
Rahul Bhargava <rahulbot@gmail.com>
2025-04-17 17:31:35 -0400

    support embed from URL on same page

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,112 @@
 <script>
+  import dayjs from 'dayjs';
+  import { onMount } from 'svelte'
+  import Map from './lib/Map.svelte';
   import Header from './lib/Header.svelte'
   import MapMaker from './lib/mapmaker/MapMaker.svelte'
   import Footer from './lib/Footer.svelte'
+  import { getData, isWithinRadius } from './lib/util/data.js';
 
-  let coords = $state([]);
+  const ACLED_URL = "2025-01-01-2025-04-10-North_America-United_States.csv";
+  const CCC_URL = "ccc-phase3-public.csv";
+
+  let urlMapSettings = $state(null);
+  let loadingData = $state(true);
+  let mapSettings = $state({
+    source: 'ACLED',
+    zoom: '8',
+    coords: [],
+    radiusMiles: '50',
+    startDate: dayjs('2025-01-01', 'YYYY-MM-DD').toDate(), // hack to get GMT date
+    endDate: new Date(), // default to today
+    width: 700,
+    height: 350
+  })
+  let data = $state({acled: [], ccc: []});   // filled in by onMount
+  let events = $derived.by(() => {
+    let allEvents = [] 
+    if (mapSettings.source == 'ACLED') {
+      allEvents = data.acled;
+    } else if (mapSettings.source == 'CCC') {
+      allEvents = data.ccc;
+    } 
+    return allEvents.filter(
+      row => isWithinRadius(mapSettings.coords[1], mapSettings.coords[0], row.lat, row.lon, mapSettings.radiusMiles)
+    );
+  });
+
+  onMount( async() => {
+    // parse URL params if in embed mode
+    const params = new URLSearchParams(window.location.search);
+    const urlParams = Object.fromEntries(params.entries());
+
+    try {
+      urlMapSettings = {
+        source: urlParams.s,
+        zoom: urlParams.z,
+        coords: urlParams.c.split(',').map(Number),
+        radiusMiles: urlParams.r,
+        startDate: dayjs(urlParams.sd, "YYYY-MM-DD").toDate(),
+        endDate: dayjs(urlParams.ed, "YYYY-MM-DD").toDate(),
+        width: urlParams.w,
+        height: urlParams.h
+      };
+    } catch (error) {
+      urlMapSettings = null;
+    }
+    if (urlMapSettings) {
+      mapSettings = urlMapSettings;
+    }
+
+    // load and normalize data from various sources
+    data.acled = await getData(ACLED_URL);
+    data.acled = data.acled.map(row => ({
+      lat: row.latitude, lon: row.longitude, date: row.event_date,
+      location: `${row.location}, ${row.admin1}`, actor: row.assoc_actor_1,
+      summary: row.notes
+    }));
+    data.ccc = await getData(CCC_URL);
+    data.ccc = data.ccc.map(row => ({
+      lat: row.lat, lon: row.lon, date: row.date,
+      location: `${row.resolved_locality}, ${row.resolved_state}`, actor: row.organizations,
+      summary: `${row.event_type} ${row.claims_summary}. About ${row.issues}.`
+    }));
+    loadingData = false;
+  
+  });
+
 </script>
 
-<Header />
+{#if loadingData }
 
-<MapMaker />
+  <div class="spinner-wrapper">
+    <div class="spinner-border" role="status"></div>
+  </div>
 
-<Footer />
+{:else}
+
+  {#if urlMapSettings != null}
+    <Map
+      zoom={mapSettings.zoom}
+      center={[mapSettings.coords[1], mapSettings.coords[0]]}
+      markers={events} 
+      width={mapSettings.width}
+      height={mapSettings.height}
+      source={mapSettings.source}
+    />
+  {:else}
+    <Header />
+    <MapMaker bind:mapSettings {events} />
+    <Footer />
+  {/if}
+
+{/if}
+
+<style>
+  .spinner-wrapper {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
--- src/lib/mapmaker/MapMaker.svelte
+++ src/lib/mapmaker/MapMaker.svelte
@@ -1,6 +1,5 @@
 <script>
   import { onMount } from 'svelte';
-  import { getData, isWithinRadius } from '../util/data.js';
   import dayjs from 'dayjs';
 
   import Navigation from './Navigation.svelte';
@@ -8,54 +7,10 @@
   import Visualize from './Visualize.svelte';
   import Embed from './Embed.svelte';
 
-  const ACLED_URL = "2025-01-01-2025-04-10-North_America-United_States.csv";
-  const CCC_URL = "ccc-phase3-public.csv";
-
-  let loadingData = $state(true);
-  let mapSettings = $state({
-    source: 'ACLED',
-    zoom: '8',
-    coords: [],
-    radiusMiles: '50',
-    startDate: dayjs('2025-01-01', 'YYYY-MM-DD').toDate(), // hack to get GMT date
-    endDate: new Date(), // default to today
-    width: 700,
-    height: 350
-  })
-  let data = $state({acled: [], ccc: []});   // filled in by onMount
-  let events = $derived.by(() => {
-    let allEvents = [] 
-    if (mapSettings.source == 'ACLED') {
-      allEvents = data.acled;
-    } else if (mapSettings.source == 'CCC') {
-      allEvents = data.ccc;
-    } 
-    return allEvents.filter(
-      row => isWithinRadius(mapSettings.coords[1], mapSettings.coords[0], row.lat, row.lon, mapSettings.radiusMiles)
-    );
-  });
-
+  let {mapSettings=$bindable(mapSettings), events} = $props();
   let step = $state(0);
 
   function updateStep(newStep) { step = newStep; }
-
-  onMount( async ()=> {
-    // load and normalize data from various sources
-    data.acled = await getData(ACLED_URL);
-    data.acled = data.acled.map(row => ({
-      lat: row.latitude, lon: row.longitude, date: row.event_date,
-      location: `${row.location}, ${row.admin1}`, actor: row.assoc_actor_1,
-      summary: row.notes
-    }));
-    data.ccc = await getData(CCC_URL);
-    data.ccc = data.ccc.map(row => ({
-      lat: row.lat, lon: row.lon, date: row.date,
-      location: `${row.resolved_locality}, ${row.resolved_state}`, actor: row.organizations,
-      summary: `${row.event_type} ${row.claims_summary}. About ${row.issues}.`
-    }));
-    loadingData = false;
-  });
-
 </script>
 
 <main>
@@ -66,21 +21,14 @@
       </div>
     </div>
 
-    {#if loadingData }
-    
-      <div class="spinner-border" role="status"></div>
-    
-    {:else}
-
-      {#if step == 0}
-        <PickData bind:mapSettings {updateStep} {events} />
-      {:else if step == 1}
-        <Visualize bind:mapSettings {updateStep} {events} />
-      {:else if step == 2}
-        <Embed {mapSettings} {events} />
-      {/if}
-    
+    {#if step == 0}
+      <PickData bind:mapSettings {updateStep} {events} />
+    {:else if step == 1}
+      <Visualize bind:mapSettings {updateStep} {events} />
+    {:else if step == 2}
+      <Embed {mapSettings} {events} />
     {/if}
+    
 
   </div>
 </main>
